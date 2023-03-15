0a944e4843a6f491374b1bd4490c85009a928550
Patrick Heneise <patrick@zentered.co>
2023-03-15 20:02:02 +0200

    feat: convert to gh action

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 export const grafanaOrg = process.env.GRAFANA_ORG
 export const grafanaApiKey = process.env.GRAFANA_CLOUD_API_KEY
+export const outputFolder = process.env.OUTPUT_FOLDER
+export const overwriteFiles = process.env.OVERWRITE_FILES === 'true'
